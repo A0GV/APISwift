b87efc47274e240f0849929b4687e546c189cb3a
A0GV <A01741767@tec.mx>
2025-11-29 16:16:10 -0600

    Agregando coordi notifs a ver si no muere

--- app/api/notificaciones.py
+++ app/api/notificaciones.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, make_response
-from ..repositories.notificaciones import obtener_notificaciones_operador
+from ..repositories.notificaciones import obtener_notificaciones_operador, obtener_notificaciones_coordi
 
 notificaciones_bp = Blueprint("notificaciones", __name__, url_prefix="/api/notificaciones")
 
@@ -15,6 +15,24 @@ def obtener_notificaciones(idOperador):
     try:
         result = obtener_notificaciones_operador(idOperador, validated_params.limite)
         
+        return make_response(jsonify(result), 200)
+        
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
+
+# GET notificaciones del coordinador
+# https://upon-quarters-feof-handbook.trycloudflare.com/api/notificaciones/coordi?limit=10
+@notificaciones_bp.route("/coordi", methods=['GET'])
+def get_notificaciones_coordi():
+    limit = request.args.get("limit", type=int)
+    
+    from models import NotificacionesCoordi
+
+    validated_params = NotificacionesCoordi(limit = limit)
+    # params = validated_params.model_dump(exclude_none=True)
+
+    try:
+        result = obtener_notificaciones_coordi(validated_params.limit)
         return make_response(jsonify(result), 200)
         
     except Exception as e:
--- app/models/notificaciones.py
+++ app/models/notificaciones.py
@@ -12,4 +12,15 @@ class NotificacionesOperador(BaseModel):
         if (value > 20):
             raise ValueError("No se pueden solicitar más de 20 notificaciones")
         return value
-        
+    
+# Para coordi
+class NotificacionesCoordi(BaseModel): 
+    limit: Optional[int] = Field(None, description="Indica cuantas notificaciones traer")
+
+    @field_validator('limit')
+    def validar_limites_notificaciones(cls, value):
+        if value is None: 
+            return 10  # Default 10 notifs
+        if value > 30:
+            raise ValueError("No se pueden solicitar más de 30 notificaciones")
+        return value
